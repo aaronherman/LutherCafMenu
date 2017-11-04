b0306c4dd416623ba377e308188539fd6c1b51c6
uetzau01 <uetzau01@luther.edu>
2017-11-04 15:35:09 -0500

    updating router AGAIN

--- router.py
+++ router.py
@@ -15,5 +15,13 @@ currentMeal = getCurrentMeal()
 def index():
     return render_template('index.html', menu=menu, date=date, week=week, currentMeal=currentMeal)
 
+@app.route('contact')
+def index():
+    return render_template('contact.html')
+
+@app.route('about')
+def index():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
