322944fa61267d47355007dac24e3d8628a86a82
uetzau01 <uetzau01@luther.edu>
2017-12-08 19:34:19 -0600

    add 'close' button and upload template

--- router.py
+++ router.py
@@ -24,5 +24,9 @@ def contact():
 def about():
     return render_template('about.html')
 
+@app.route('/upload')
+def upload():
+	return render_template('upload.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
